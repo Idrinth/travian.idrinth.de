b191e5b9da9bec82a861bcb96165bd1ed7d37f28
Björn Büttner <Idrinth@users.noreply.github.com>
2022-01-11 17:10:56 +0100

    add attack parser

---
+++ src/AttackParser.php
@@ -0,0 +1,147 @@
+<?php
+
+namespace De\Idrinth\Travian;
+
+use DOMDocument;
+use UnexpectedValueException;
+
+class AttackParser
+{
+    private $twig;
+    private $time;
+    private $distance;
+    public function __construct(Twig $twig, TravelTime $time, DistanceCalculator $distance)
+    {
+        $this->twig = $twig;
+        $this->time = $time;
+        $this->distance = $distance;
+    }
+    private function getMapSize(DOMDocument $doc): array
+    {
+        $scripts = $doc->getElementsByTagName('script');
+        for ($i = 0; $i < $scripts->length; $i++) {
+            if (preg_match('/window.TravianDefaults\s+=/', $scripts->item($i)->textContent)) {
+                preg_match('/"width":([\-0-9]+),"height":([\-0-9]+)/', $scripts->item($i)->textContent, $matches);
+                return [
+                    'width' => intval($matches[1], 10),
+                    'height' => intval($matches[2], 10),
+                ];
+            }
+        }
+        throw new UnexpectedValueException('Couldn\'t find map size');
+    }
+    private function mayTravelOverMapBorder(DOMDocument $doc): bool
+    {
+        $scripts = $doc->getElementsByTagName('script');
+        for ($i = 0; $i < $scripts->length; $i++) {
+            if (preg_match('/"travelOverTheWorldEdge":true/', $scripts->item($i)->textContent)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    private function getVillage(DOMDocument $doc): array
+    {
+        $divs = $doc->getElementById('sidebarBoxVillagelist')->getElementsByTagName('div');
+        for ($i=0; $i < $divs->length; $i++) {
+            if (in_array('villageList', explode(' ', $divs->item($i)->attributes->getNamedItem('class')->textContent), true)) {
+                $as = $divs->item($i)->getElementsByTagName('a');
+                for ($j=0; $j < $as->length; $j++) {
+                    $span = $as->item($j)->nextSibling->nextSibling;
+                    if (in_array('active', explode(' ', $span->parentNode->attributes->getNamedItem('class')->textContent))) {
+                        return [
+                            'name' => $span->attributes->getNamedItem('data-villagename')->textContent,
+                            'x' => intval($span->attributes->getNamedItem('data-x')->textContent, 10),
+                            'y' => intval($span->attributes->getNamedItem('data-y')->textContent, 10),
+                        ];
+                    }
+                }
+            }
+        }
+        throw new UnexpectedValueException('No Village found');
+    }
+    private function getCoords(\DOMElement $table): array
+    {
+        for ($j = 0; $j < $table->childNodes->length; $j++) {
+            $tbody = $table->childNodes->item($j);
+            if ($tbody->localName === 'tbody') {
+                if ($tbody->attributes->getNamedItem('class') && $tbody->attributes->getNamedItem('class')->textContent === 'units') {
+                    for ($k = 0; $k < $tbody->childNodes->length; $k++) {
+                        $tr = $tbody->childNodes->item($k);
+                        if ($tr->localName === 'tr') {
+                            for ($l = 0; $l < $tr->childNodes->length; $l++) {
+                                $th = $tr->childNodes->item($l);
+                                if ($th->localName === 'th') {
+                                    if ($th->attributes->getNamedItem('class')->textContent === 'coords') {
+                                        return explode('|', (preg_replace('#[^0-9|\-]#', '', str_replace('−', '-', $th->textContent))));
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    private function getDuration(\DOMElement $table): int
+    {
+        for ($j = 0; $j < $table->childNodes->length; $j++) {
+            $tbody = $table->childNodes->item($j);
+            if ($tbody->attributes->getNamedItem('class') && $tbody->attributes->getNamedItem('class')->textContent === 'infos') {
+                for ($k = 0; $k < $tbody->childNodes->length; $k++) {
+                    $tr = $tbody->childNodes->item($k);
+                    if ($tr->localName === 'tr') {
+                        for ($l = 0; $l < $tr->childNodes->length; $l++) {
+                            $td = $tr->childNodes->item($l);
+                            if ($td->localName === 'td') {
+                                for ($m = 0; $m < $td->childNodes->length; $m++) {
+                                    $div = $td->childNodes->item($m);
+                                    if ($div->localName === 'div' && $div->attributes->getNamedItem('class')->textContent === 'in') {
+                                        for ($n = 0; $n < $div->childNodes->length; $n++) {
+                                            $span = $div->childNodes->item($n);
+                                            if ($span->localName === 'span') {
+                                                return intval($span->attributes->getNamedItem('value')->textContent, 10);
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }        
+    }
+    public function run($post)
+    {
+        $data = [];
+        if (isset($post['source']) && $post['source']) {
+            $doc = new DOMDocument();
+            $doc->loadHTML($post['source']);
+            $blindTime = explode(':', $post['blind_time']);
+            $blindTime = $blindTime[0]*3600 + $blindTime[1]*60 + $blindTime[2];
+            $mayTravelOverBorder = $this->mayTravelOverMapBorder($doc);
+            $size = $this->getMapSize($doc);
+            $village = $this->getVillage($doc);
+            $tables = $doc->getElementsByTagName('table');
+            for ($i = 0; $i < $tables->length; $i++) {
+                if (in_array('inAttack', explode(' ', $tables->item($i)->attributes->getNamedItem('class')->textContent))) {
+                    $coords = $this->getCoords($tables->item($i));
+                    $duration = $this->getDuration($tables->item($i));
+                    $distance = $this->distance->distance(new Point($coords[0], $coords[1]), new Point($village['x'], $village['y']), $size['width'], $size['height'], $mayTravelOverBorder);
+                    $row = $this->time->find($distance, 0, 0, $duration, $blindTime);
+                    $data['attacks'][] = [
+                        'from' => [
+                            'x' => $coords[0],
+                            'y' => $coords[1],
+                        ],
+                        'to' => $village,
+                        'distance' => $distance,
+                        'units' => $row,
+                    ];
+                }
+            }
+        }
+        $this->twig->display('attack-parser.twig', $data);
+    }
+}
--- src/TravelTime.php
+++ src/TravelTime.php
@@ -4,21 +4,20 @@ namespace De\Idrinth\Travian;
 
 class TravelTime
 {
-    private static $speed = [];
-    public function find(float $distance, float $shoes, float $map, int $duration, int $blindTime)
+    public function find(float $distance, int $shoes, int $map, int $duration, int $blindTime)
     {
         $result = [];
-        foreach(self::$speed as $speed => $units) {
+        foreach(Troops::SPEED as $unit => $speed) {
             for ($i=0; $i<=20; $i++) {
-                if ($shoes !== 0 || $map !== 0) {
-                    $time = $this->time($distance, $speed, 0, $i, $shoes, $map);
-                    if ($time[0] >= $duration && $time[0] <= $duration + $blindTime) {
-                        $result[] = [$speed, $i, $units, $time, 'with hero'];
-                    }
-                }
-                $time = $this->time($post['distance'], $speed, 0, $i, 0, 0);
+                $time = $this->time($distance, $speed, 0, $i, 0, 0);
                 if ($time[0] >= $duration && $time[0] <= $duration + $blindTime) {
-                    $result[] = [$speed, $i, $units, $time, ''];
+                    $result[$unit] = $result[$unit] ?? [$speed, $i, $unit, $time, ''];
+                }
+                if ($shoes !== 0 || $map !== 0) {
+                    $time = $this->time($distance, $speed, 0, $i, 1 - $shoes/100, 1 - $map/100);
+                    if ($time[0] >= $duration && $time[0] <= $duration + $blindTime) {
+                        $result[$unit] = $result[$unit] ?? [$speed, $i, $unit, $time, 'with hero'];
+                    }
                 }
             }
         }
