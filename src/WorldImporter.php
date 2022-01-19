e6f00aa37993b6be59f63f0fff5f3559c9837085
Björn Büttner <Idrinth@users.noreply.github.com>
2022-01-19 19:03:27 +0100

    new database structure for imports

--- src/WorldImporter.php
+++ src/WorldImporter.php
@@ -60,6 +60,49 @@ class WorldImporter
                 $this->database->exec('CREATE TABLE `' . $world . '` LIKE x_world');
                 $this->database->exec('INSERT INTO `' . $world . '` SELECT * FROM x_world');
                 $this->database->exec('TRUNCATE x_world');
+                foreach ($this->database->query("SELECT DISTINCT alliance_id,alliance_name FROM `$world` WHERE alliance_id<>0")->fetchAll(PDO::FETCH_ASSOC) as $row) {
+                    $stmt = $this->database->prepare('SELECT aid,name FROM world_alliances WHERE id=:id AND world=:world');
+                    $stmt->execute([':world' => $world, ':id' => $row['alliance_id']]);
+                    list($id, $name) = $stmt->fetch(PDO::FETCH_NUM);
+                    if (!$id || $row['alliance_name'] !== $name) {
+                        $this->database
+                            ->prepare('INSERT INTO world_alliances (id,world,name,`from`,`until`) VALUES(:id,:world,:name,:today,:today)')
+                            ->execute([':name' => $row['alliance_name'], ':id' => $row['alliance_id'], ':world' => $world, ':today' => date('Y-m-d')]);
+                    } else {
+                        $this->database
+                            ->prepare('UPDATE world_alliances SET until=:today WHERE aid=:id')
+                    ->execute([':id' => $id, ':today' => date('Y-m-d')]);
+                    }
+                }
+                foreach ($this->database->query("SELECT DISTINCT player_id,player_name,alliance_id FROM `$world`")->fetchAll(PDO::FETCH_ASSOC) as $row) {
+                    $stmt = $this->database->prepare('SELECT aid,name,alliance FROM world_players WHERE id=:id AND world=:world');
+                    $stmt->execute([':world' => $world, ':id' => $row['player_id']]);
+                    list($id, $name, $alliance) = $stmt->fetch(PDO::FETCH_NUM);
+                    if (!$id || $row['player_name'] !== $name || $alliance !== $row['alliance_id']) {
+                        $this->database
+                            ->prepare('INSERT INTO world_players (id,alliance,world,name,`from`,`until`) VALUES(:id,:alliance,:world,:name,:today,:today)')
+                            ->execute([':name' => $row['player_name'], ':id' => $row['player_id'], ':alliance' => $row['alliance_id'], ':world' => $world, ':today' => date('Y-m-d')]);
+                    } else {
+                        $this->database
+                            ->prepare('UPDATE world_players SET until=:today WHERE aid=:id')
+                    ->execute([':id' => $id, ':today' => date('Y-m-d')]);
+                    }
+                }
+                foreach ($this->database->query("SELECT DISTINCT tribe,village_id,player_id,population,village_name,x,y FROM `$world`")->fetchAll(PDO::FETCH_ASSOC) as $row) {
+                    $this->database
+                        ->prepare('INSERT INTO world_villages (tribe,population,id,x,y,world,name,day,player) VALUES(:tribe,:population,:id,:x,:y,:world,:name,:today,:player)')
+                        ->execute([
+                            ':tribe' => [1 => 'roman', 2 => 'teuton', 3 => 'gaul', 5 => 'natar', 6 => 'egyptian', 7 => 'hun'][intval($row['tribe'], 10)],
+                            ':population' => $row['population'],
+                            ':name' => $row['village_name'],
+                            ':x' => $row['x'],
+                            ':y' => $row['y'],
+                            ':id' => $row['village_id'],
+                            ':player' => $row['player_id'],
+                            ':world' => $world,
+                            ':today' => date('Y-m-d')
+                        ]);
+                }
             });
         }
         $multicurl->start();
