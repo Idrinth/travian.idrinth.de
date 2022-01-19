62e3950247cc29056083de477891fca1ccb72ef3
Björn Büttner <Idrinth@users.noreply.github.com>
2022-01-19 08:21:02 +0100

    only update updated when successful

--- src/WorldImporter.php
+++ src/WorldImporter.php
@@ -40,12 +40,12 @@ class WorldImporter
         $stmt->execute([':today' => date('Y-m-d H:i:s', time() - 86400), ':yesterday' => date('Y-m-d H:i:s', time() - 86400*2)]);
         $multicurl = new MultiCurl();
         foreach ($stmt->fetchAll(PDO::FETCH_ASSOC) as $row) {
-            $this->database
-                ->prepare('UPDATE world_updates SET updated=:now WHERE world=:world')
-                ->execute([':now' => date('Y-m-d H:i:s'), ':world' => $row['world']]);
             $multicurl->addGet('https://'.$row['world'].'/map.sql')->success(function(Curl $curl) use($row) {
-                $this->database->exec('TRUNCATE x_world');
                 $world = $row['world'];
+                $this->database
+                    ->prepare('UPDATE world_updates SET updated=:now WHERE world=:world')
+                    ->execute([':now' => date('Y-m-d H:i:s'), ':world' => $world]);
+                $this->database->exec('TRUNCATE x_world');
                 foreach(explode("\n", $curl->response) as $row) {
                     if ($row) {
                         $this->database->exec(str_replace([',FALSE,', ',TRUE,'], [',0,', ',1,'], $row));
