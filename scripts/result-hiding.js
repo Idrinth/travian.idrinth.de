91e629125a7ea02b34002c1130cea62958402545
Björn Büttner <Idrinth@users.noreply.github.com>
2022-02-09 22:02:49 +0100

    improve result handling

--- scripts/result-hiding.js
+++ scripts/result-hiding.js
@@ -2,5 +2,11 @@
     const els = document.getElementsByClassName('result');
     for (let i=0; i< els.length; i++) {
         els[i].classList.toggle('hidden');
+        els[i].getElementsByTagName('h3')[0].onclick = () => {
+            if (event && event.originalTarget.parentNode !== els[i]) {
+                return;
+            }
+            els[i].classList.toggle('hidden');
+        };
     }
 })();
