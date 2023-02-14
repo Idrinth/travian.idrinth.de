eaaa24409cce7bf2906e5c1d4baf812ec230606d
Björn Büttner <Idrinth@users.noreply.github.com>
2023-02-14 10:25:14 +0100

    fix parameter name

--- src/Page/TroopTool.php
+++ src/Page/TroopTool.php
@@ -180,7 +180,7 @@ AND y=:y");
                 $stmt->execute([':aid' => $aid]);
                 $world = $stmt->fetch(\PDO::FETCH_COLUMN);
                 $stmt = $this->database->prepare('SELECT IF(`main`,`user`,`dual`) as id FROM user_world.`user`=:id AND user_world.world=:world');
-                $stmt->execute([':user' => $_SESSION['id'], ':world' => $world,]);
+                $stmt->execute([':id' => $_SESSION['id'], ':world' => $world,]);
                 $user = intval($stmt->fetch(\PDO::FETCH_COLUMN), 10) ?: $_SESSION['id'];
                 $this->database
                     ->prepare("UPDATE troops "
