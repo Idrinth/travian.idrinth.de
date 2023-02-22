ae16a36586b11afa9620de67057e4574ca3075dc
Björn Büttner <Idrinth@users.noreply.github.com>
2023-02-22 15:50:14 +0100

    try with reasonable path

--- src/Resource/Styles.php
+++ src/Resource/Styles.php
@@ -10,11 +10,10 @@ class Styles
     public function run(): void
     {
         header('Content-type: text/css');
-        $root = dirname(__DIR, 2);
         echo (new CSS())->minify((new Compiler(['cacheDir' => dirname(__DIR__, 2).'/cache']))
             ->compileString(
                 //somehow this ends up in the public dir, so we have to manually change the imports
-                '@import("' . $root . '/styles/normalize.scss");@import("' . $root . '/styles/styles.scss");',
+                '@import("styles/normalize.scss");@import("styles/styles.scss");',
                 dirname(__DIR__, 2)
             )
             ->getCss());
