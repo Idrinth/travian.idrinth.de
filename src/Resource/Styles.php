d20ee7fa8874e8d514382fe8251ced3b5802b893
Björn Büttner <Idrinth@users.noreply.github.com>
2023-02-22 15:47:42 +0100

    make path absolute

--- src/Resource/Styles.php
+++ src/Resource/Styles.php
@@ -10,10 +10,11 @@ class Styles
     public function run(): void
     {
         header('Content-type: text/css');
+        $root = dirname(__DIR, 2);
         echo (new CSS())->minify((new Compiler(['cacheDir' => dirname(__DIR__, 2).'/cache']))
             ->compileString(
                 //somehow this ends up in the public dir, so we have to manually change the imports
-                '@import("../styles/normalize.scss");@import("../styles/styles.scss");',
+                '@import("' . $root . '/styles/normalize.scss");@import("' . $root . '/styles/styles.scss");',
                 dirname(__DIR__, 2)
             )
             ->getCss());
