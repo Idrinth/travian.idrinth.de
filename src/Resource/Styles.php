f4297a3bbbf9bdc30929965cc2b1cde93ae45706
Björn Büttner <Idrinth@users.noreply.github.com>
2023-02-22 15:53:50 +0100

    separate minify and parse

--- src/Resource/Styles.php
+++ src/Resource/Styles.php
@@ -10,12 +10,14 @@ class Styles
     public function run(): void
     {
         header('Content-type: text/css');
-        echo (new CSS())->minify((new Compiler(['cacheDir' => dirname(__DIR__, 2).'/cache']))
+        $css = (new Compiler(['cacheDir' => dirname(__DIR__, 2).'/cache']))
             ->compileString(
                 //somehow this ends up in the public dir, so we have to manually change the imports
                 '@import("styles/normalize.scss");@import("styles/styles.scss");',
                 dirname(__DIR__, 2)
             )
-            ->getCss());
+            ->getCss();
+        echo $css;
+        echo (new CSS())->minify($css);
     }
 }
