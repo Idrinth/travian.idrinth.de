b0008c1de5d9ce9da20b8b2a98b71100c79c16ef
Björn Büttner <Idrinth@users.noreply.github.com>
2026-03-28 15:27:29 +0100

    yaml facade is not compatible with php 8.3

--- src/Translations.php
+++ src/Translations.php
@@ -2,16 +2,16 @@
 
 namespace De\Idrinth\Travian;
 
-use De\Idrinth\Yaml\Yaml;
+use Symfony\Component\Yaml\Yaml
 
 class Translations
 {
     public static function get(string $language): array
     {
         $dir = dirname(__DIR__) . '/translations';
-        $data = (array) Yaml::decodeFromFile($dir . '/en.yml');
+        $data = (array) Yaml::parseFile($dir . '/en.yml');
         if (strlen($language) === 2 && $language !== 'en' && is_file($dir . '/' . $language . '.yml')) {
-            foreach ((array) Yaml::decodeFromFile($dir . '/' . $language . '.yml') as $key => $value) {
+            foreach ((array) Yaml::parseFile($dir . '/' . $language . '.yml') as $key => $value) {
                 $data[$key] = $value;
             }
         }
